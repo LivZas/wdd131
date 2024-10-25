5b5f9ca1e9f370aaed71f6fcee457d295660d885
LivZas <triridiscent@gmail.com>
2024-10-25 12:27:45 -0400

    1

---
+++ project/scripts/home.js
@@ -0,0 +1,13 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navigation = document.querySelector('.navigation');
+
+menuToggle.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+    menuToggle.textContent = navigation.classList.contains('active') ? '✖' : '☰';
+});
